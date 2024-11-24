3a497b42dc213636c25cedfde2e2b00c26351aeb
Markus Löffler <markus.loeffler01@gmail.com>
2024-11-24 19:56:28 +0100

    Initial

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+
+
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import react from "eslint-plugin-react";
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react': react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      // This will warn about stale closures in useEffect/useCallback
+      "react-hooks/exhaustive-deps": "warn",
+      // This prevents storing components in variables (related pattern)
+      "react/jsx-no-constructed-context-values": "error",
+    
+      // This ensures you're not creating components inside components
+      "react/no-unstable-nested-components": "error",
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+)
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": "/src",
+      "@components": path.resolve("./src/components"),
+      "@cmain": path.resolve("./src/components/main"),
+      "@clist": path.resolve("./src/components/list"),
+      "@cexport": path.resolve("./src/components/export"),
+      "@cdetail": path.resolve("./src/components/detail"),
+      "@pages": path.resolve("./src/pages"),
+      "@stores": path.resolve("./src/stores"),
+      "@type": path.resolve("./src/types"),
+      "@ts": path.resolve("./src/ts"),
+      "@utils": path.resolve("./src/utils"),
+    }
+  }
+})
